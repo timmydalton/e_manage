b613ce41f81584aef5188e80918e9b9063627027
KhanhDuyTr <timmydalton2k@gmail.com>
2023-09-09 11:53:19 +0700

    imp

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router"
+import { useCookies } from "vue3-cookies";
+const { cookies } = useCookies();
 
 const routes = [
   {
@@ -11,5 +13,22 @@ const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  if(to.path != '/login') {
+    if(checkToken()) { 
+        next();
+    } else {
+        next('login');
+    }
+  } else {
+      next(); // This is where it should have been
+  }
+})
+
+function checkToken() {
+  let access_token = cookies.get('access_token')
+  return false
+}
 
 export default router
